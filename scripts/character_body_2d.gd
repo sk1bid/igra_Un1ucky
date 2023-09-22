37e29339846600fdc7e07b3ee54c6ebcc5c740c5
sk1bid <ivanov.artem.bld@gmail.com>
2023-09-22 18:56:35 +0700

    0.0.03

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -13,9 +13,40 @@ enum PLAYER_STATE { IDLE, RUN, JUMP, FALL}
 var _state: PLAYER_STATE = PLAYER_STATE.IDLE
 
 func _physics_process(delta):
-	apply_gravity()
+	apply_gravity(delta)
+	move_horizontal()
 	move_and_slide()
+	jump_handler()
+
+
+func apply_gravity(delta):
+	if !is_on_floor():
+		
+		velocity.y += GRAVITY * delta
+		
+
+
+
+
 func move_horizontal():
 	velocity.x = 0
 	
-	if Input.is_action_pressed()
+	if Input.is_action_pressed('move_left'):
+		velocity.x -= RUN_SPEED
+	elif Input.is_action_pressed('move_right'):
+		velocity.x += RUN_SPEED
+		
+		
+func jump_handler():
+	if Input.is_action_just_pressed('jump') and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+func clamp_fall_velocity(): 
+	velocity.y = clampf(velocity.y , JUMP_VELOCITY, MAX_FALL)
+	
+		
+		
+	
+func set_state(new_state: PLAYER_STATE):
+	if new_state == state:
+	
